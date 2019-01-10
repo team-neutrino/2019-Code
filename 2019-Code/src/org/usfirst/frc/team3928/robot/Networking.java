7a235d1db64c87655db85233d5b8d7bf82578ea0
JoelNeppel <joelneppel@gmail.com>
2019-01-10 17:16:27 -0600

    Push

---
+++ 2019-Code/src/org/usfirst/frc/team3928/robot/Networking.java
@@ -0,0 +1,20 @@
+package org.usfirst.frc.team3928.robot;
+
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import org.usfirst.frc.team3928.robot.Robot;
+
+public class Networking {	
+	
+	public static NetworkTableInstance instance = NetworkTableInstance.create();
+	public static NetworkTableEntry entry = new NetworkTableEntry(instance, 5800);
+	private static byte[] defaultArray = new byte[0];
+	
+	public static byte[] getBytes() {
+		
+		return entry.getRaw(defaultArray);
+	
+	}
+
+}
