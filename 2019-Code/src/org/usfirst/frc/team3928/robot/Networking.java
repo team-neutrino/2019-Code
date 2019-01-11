72bb983d1bd8ac9198b65915314d89fb93908b03
JoelNeppel <joelneppel@gmail.com>
2019-01-10 18:56:05 -0600

    "Yeeeet" - Indy

--- 2019-Code/src/org/usfirst/frc/team3928/robot/Networking.java
+++ 2019-Code/src/org/usfirst/frc/team3928/robot/Networking.java
@@ -3,6 +3,9 @@ package org.usfirst.frc.team3928.robot;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
+
+import java.util.Arrays;
+
 import org.usfirst.frc.team3928.robot.Robot;
 
 public class Networking {	
@@ -14,13 +17,15 @@ public class Networking {
 	public static void sendBytes(byte[] bytes) {
 		
 		entry.forceSetRaw(bytes);
+		System.out.println("Set " + Arrays.toString(bytes));
 		
 	}
 	
 	public static byte[] getBytes() {
 		
+		System.out.println("Returned " + Arrays.toString(entry.getRaw(defaultArray)));
 		return entry.getRaw(defaultArray);
-	
+		
 	}
 
 }
