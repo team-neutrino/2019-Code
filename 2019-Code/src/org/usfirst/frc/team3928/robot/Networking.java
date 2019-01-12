c17ddc46e0e30712d1797b55102054e7e51b1385
JoelNeppel <joelneppel@gmail.com>
2019-01-12 10:27:13 -0600

    Using NetworkTable instead of NetworkTableEntry

--- 2019-Code/src/org/usfirst/frc/team3928/robot/Networking.java
+++ 2019-Code/src/org/usfirst/frc/team3928/robot/Networking.java
@@ -11,21 +11,22 @@ import org.usfirst.frc.team3928.robot.Robot;
 public class Networking {	
 	
 	public static NetworkTableInstance instance = NetworkTableInstance.getDefault();
+	public static NetworkTable table = instance.getTable("networkingTest");
 	public static NetworkTableEntry entry = new NetworkTableEntry(instance, 5800);
 	public static byte[] defaultArray = new byte[]{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
 	
 	public static void sendBytes(byte[] bytes) {
 		
-		entry.setRaw(bytes);
-		System.out.println("Set " + Arrays.toString(bytes));
+		table.getKeys().add(bytes.toString());
+		System.out.println("Set " + bytes.toString());
 		
 	}
 	
 	public static byte[] getBytes() {
 		
-		System.out.println("Returned " + Arrays.toString(entry.getRaw(defaultArray)));
+		System.out.println("Returned " + table.getKeys().toString());
 		
-		return entry.getRaw(defaultArray);
+		return table.getKeys().toString().getBytes();
 		
 	}
 
