c5f75a0afdfb4f814b890986a158ca2e0be6c5f1
Veecy82 <32604484+Veecy82@users.noreply.github.com>
2019-01-15 18:53:39 -0600

    Updated Odometry

--- 2019-Code/src/org/usfirst/frc/team3928/robot/Odometry.java
+++ 2019-Code/src/org/usfirst/frc/team3928/robot/Odometry.java
@@ -9,9 +9,31 @@ import edu.wpi.first.wpilibj.Encoder;
 
 public class Odometry {
 
-	public static Encoder encoder;
 	public static AHRS navx;	
+	public static double positionX;
+	public static double positionY;
+	//Average of the distance travelled by the two wheels since the last time it was calculated
+	double totalDistance = 0;
+	//Distance travelled by the left wheel, as found by the previous calculation
+	double formerLeftDistance = 0;
+	//Distance travelled by the right wheel, as found by the previous calculation
+	double formerRightDistance = 0;
+	//Distance travelled by the left wheel since the previous calculation
+	double currentLeftDistance = 0;
+	//Distance travelled by the left wheel since the previous calculation
+	double currentRightDistance = 0;
 	
-	
+	public void calculateDistance(){
+
+		currentRightDistance = Drive.rightEncoder.distance-formerRightDistance;
+		currentLeftDistance = Drive.leftEncoder.distance-formerLeftDistance;
+		formerLeftDistance = Drive.leftEncoder.distance;
+		formerRightDistance = Drive.rightEncoder.distance;
+		totalDistance = (currentLeftDistance + currentRightDistance)/2;
+		positionX += totalDistance * Math.Sin(navx.GetYaw);
+		positionY += totalDistance * Math.Cos(navx.GetYaw);
+		System.out.println(positionX + ", " + positionY);
+
+	}
 	
 }
